aec3379b0e64943f2c941a0f77fa46a022e91436
tonizhuravlevxd-blip <toni.zhuravlev.xd@mail.ru>
2026-04-07 15:58:24 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -1810,7 +1810,12 @@ import logging
 # ================= CALLBACK =================
 async def button_handler(update: Update, context: ContextTypes.DEFAULT_TYPE):
     query = update.callback_query
-    await query.answer()
+
+    try:
+        await query.answer()
+    except:
+        pass
+
     data = query.data
     user_id = query.from_user.id
 
@@ -2511,7 +2516,6 @@ app.add_handler(CommandHandler("sos", sos_handler))
 
 
 app.add_handler(CallbackQueryHandler(button_handler))
-app.add_handler(CallbackQueryHandler(support_reply_button, pattern="^reply_"))
 app.add_handler(PreCheckoutQueryHandler(pre_checkout))
 app.add_handler(MessageHandler(filters.PHOTO, handle_photo))
 app.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND, handle_text))
